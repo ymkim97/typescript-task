360d67725bd46c8fea4a81c551a68e0a95d2c477
ymkim97 <rladudaud97@naver.com>
2024-08-20 21:57:06 +0900

    config: 환경변수 file 추가

---
+++ src/config/index.ts
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+const env = dotenv.config();
+if (env.error) throw new Error('No .env file found.');
+
+export default {
+  nodeEnv: process.env.NODE_ENV,
+
+  serverPort: process.env.PORT,
+
+  log: {
+    level: process.env.LOG_LEVEL || 'info',
+  },
+
+  mysql: {
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    db: process.env.MYSQL_DB,
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+  },
+};
